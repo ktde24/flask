15a9b6d1101a627c86e4ede8a776cf962a4219dc
ktde24 <118182432+ktde24@users.noreply.github.com>
2024-01-15 10:50:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return "Hello, Webapp!"
     
 if __name__ == '__main__':
     app.run('0.0.0.0', 80, True) # port를 80으로
